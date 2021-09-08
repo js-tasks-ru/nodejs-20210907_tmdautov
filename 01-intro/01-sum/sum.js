ce025dab06f8a5d4fc50918f06a9e15e430b5c84
tmdautov <tdautov@klika-tech.com>
2021-09-08 13:01:03 +0600

    fix(): remove parseInt

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -2,7 +2,7 @@ function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError();
   }
-  return parseInt(a) + parseInt(b);
+  return a + b;
 }
 
 module.exports = sum;
